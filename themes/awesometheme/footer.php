e39dd413bd484377d3ba80b203aad3018250bd77
Steven Halase <steven@nsrit.com>
2016-08-31 17:20:12 -0500

    Initial Commit

---
+++ themes/awesometheme/footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        <p>This is the footer</p>
+    </footer>
+
+    <?php wp_footer() ?>
+  </body>
+</html>
---
+++ themes/awesometheme/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Awesome theme</title>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
